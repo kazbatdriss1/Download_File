7021c07c51bed0d0c9e47b81a9d52e853e71902c
kazbatdriss1 <kazbatdriss81@gmail.com>
2022-10-31 22:54:06 +0100

    Ajoutez des fichiers projet.

---
+++ Download_File/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Download_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_url = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBox_url = new Guna.UI2.WinForms.Guna2TextBox();
+            this.ProgressBar = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.guna2GradientButton1 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2GradientButton2 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2GradientButton3 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.label_ProgressBar = new System.Windows.Forms.Label();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label_Downloded = new System.Windows.Forms.Label();
+            this.label_Speed = new System.Windows.Forms.Label();
+            this.guna2CustomGradientPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Controls.Add(this.panel1);
+            this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2CustomGradientPanel1.FillColor = System.Drawing.Color.Magenta;
+            this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(0)))), ((int)(((byte)(121)))));
+            this.guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.MediumPurple;
+            this.guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(105)))), ((int)(((byte)(175)))));
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.ShadowDecoration.Parent = this.guna2CustomGradientPanel1;
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(683, 295);
+            this.guna2CustomGradientPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label_Speed);
+            this.panel1.Controls.Add(this.label_Downloded);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label_Status);
+            this.panel1.Controls.Add(this.label_ProgressBar);
+            this.panel1.Controls.Add(this.guna2GradientButton3);
+            this.panel1.Controls.Add(this.guna2GradientButton2);
+            this.panel1.Controls.Add(this.guna2GradientButton1);
+            this.panel1.Controls.Add(this.ProgressBar);
+            this.panel1.Controls.Add(this.TextBox_url);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label_url);
+            this.panel1.Location = new System.Drawing.Point(32, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 249);
+            this.panel1.TabIndex = 1;
+            // 
+            // label_url
+            // 
+            this.label_url.AutoSize = true;
+            this.label_url.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_url.Location = new System.Drawing.Point(34, 27);
+            this.label_url.Name = "label_url";
+            this.label_url.Size = new System.Drawing.Size(40, 18);
+            this.label_url.TabIndex = 0;
+            this.label_url.Text = "Url :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Status :";
+            // 
+            // TextBox_url
+            // 
+            this.TextBox_url.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TextBox_url.DefaultText = "";
+            this.TextBox_url.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TextBox_url.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TextBox_url.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBox_url.DisabledState.Parent = this.TextBox_url;
+            this.TextBox_url.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBox_url.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBox_url.FocusedState.Parent = this.TextBox_url;
+            this.TextBox_url.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBox_url.HoverState.Parent = this.TextBox_url;
+            this.TextBox_url.Location = new System.Drawing.Point(90, 18);
+            this.TextBox_url.Name = "TextBox_url";
+            this.TextBox_url.PasswordChar = '\0';
+            this.TextBox_url.PlaceholderText = "";
+            this.TextBox_url.SelectedText = "";
+            this.TextBox_url.ShadowDecoration.Parent = this.TextBox_url;
+            this.TextBox_url.Size = new System.Drawing.Size(490, 27);
+            this.TextBox_url.TabIndex = 2;
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Horizontal;
+            this.ProgressBar.Location = new System.Drawing.Point(91, 107);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(221)))), ((int)(((byte)(163)))));
+            this.ProgressBar.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(221)))), ((int)(((byte)(163)))));
+            this.ProgressBar.ShadowDecoration.Parent = this.ProgressBar;
+            this.ProgressBar.Size = new System.Drawing.Size(448, 27);
+            this.ProgressBar.TabIndex = 3;
+            this.ProgressBar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // guna2GradientButton1
+            // 
+            this.guna2GradientButton1.CheckedState.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.CustomImages.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.Font = new System.Drawing.Font("Georgia", 12F);
+            this.guna2GradientButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton1.HoverState.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.Location = new System.Drawing.Point(90, 191);
+            this.guna2GradientButton1.Name = "guna2GradientButton1";
+            this.guna2GradientButton1.ShadowDecoration.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.Size = new System.Drawing.Size(111, 37);
+            this.guna2GradientButton1.TabIndex = 4;
+            this.guna2GradientButton1.Text = "Start";
+            this.guna2GradientButton1.Click += new System.EventHandler(this.guna2GradientButton1_Click);
+            // 
+            // guna2GradientButton2
+            // 
+            this.guna2GradientButton2.CheckedState.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.CustomImages.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.Font = new System.Drawing.Font("Georgia", 12F);
+            this.guna2GradientButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton2.HoverState.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.Location = new System.Drawing.Point(281, 191);
+            this.guna2GradientButton2.Name = "guna2GradientButton2";
+            this.guna2GradientButton2.ShadowDecoration.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.Size = new System.Drawing.Size(111, 37);
+            this.guna2GradientButton2.TabIndex = 5;
+            this.guna2GradientButton2.Text = "Pause";
+            this.guna2GradientButton2.Click += new System.EventHandler(this.guna2GradientButton2_Click);
+            // 
+            // guna2GradientButton3
+            // 
+            this.guna2GradientButton3.CheckedState.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.CustomImages.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.Font = new System.Drawing.Font("Georgia", 12F);
+            this.guna2GradientButton3.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton3.HoverState.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.Location = new System.Drawing.Point(468, 191);
+            this.guna2GradientButton3.Name = "guna2GradientButton3";
+            this.guna2GradientButton3.ShadowDecoration.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.Size = new System.Drawing.Size(111, 37);
+            this.guna2GradientButton3.TabIndex = 6;
+            this.guna2GradientButton3.Text = "Resume";
+            this.guna2GradientButton3.Click += new System.EventHandler(this.guna2GradientButton3_Click);
+            // 
+            // label_ProgressBar
+            // 
+            this.label_ProgressBar.AutoSize = true;
+            this.label_ProgressBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ProgressBar.Location = new System.Drawing.Point(544, 110);
+            this.label_ProgressBar.Name = "label_ProgressBar";
+            this.label_ProgressBar.Size = new System.Drawing.Size(36, 20);
+            this.label_ProgressBar.TabIndex = 7;
+            this.label_ProgressBar.Text = "0 %";
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Status.Location = new System.Drawing.Point(88, 67);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(32, 18);
+            this.label_Status.TabIndex = 8;
+            this.label_Status.Text = "???";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(87, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Downloded :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(429, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Speed :";
+            // 
+            // label_Downloded
+            // 
+            this.label_Downloded.AutoSize = true;
+            this.label_Downloded.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label_Downloded.Location = new System.Drawing.Point(188, 152);
+            this.label_Downloded.Name = "label_Downloded";
+            this.label_Downloded.Size = new System.Drawing.Size(46, 20);
+            this.label_Downloded.TabIndex = 11;
+            this.label_Downloded.Text = "0 MB";
+            // 
+            // label_Speed
+            // 
+            this.label_Speed.AutoSize = true;
+            this.label_Speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Speed.Location = new System.Drawing.Point(516, 150);
+            this.label_Speed.Name = "label_Speed";
+            this.label_Speed.Size = new System.Drawing.Size(58, 20);
+            this.label_Speed.TabIndex = 12;
+            this.label_Speed.Text = "0 MB/s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(683, 295);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_Speed;
+        private System.Windows.Forms.Label label_Downloded;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_Status;
+        private System.Windows.Forms.Label label_ProgressBar;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton3;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton2;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton1;
+        private Guna.UI2.WinForms.Guna2ProgressBar ProgressBar;
+        private Guna.UI2.WinForms.Guna2TextBox TextBox_url;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_url;
+    }
+}
+
---
+++ Download_File/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AltoHttp;
+
+
+namespace Download_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HttpDownloader httpDownloader;
+        private void guna2GradientButton1_Click(object sender, EventArgs e)
+        {
+            httpDownloader = new HttpDownloader(TextBox_url.Text,$"{Application.StartupPath}\\{Path.GetFileName(TextBox_url.Text)}");
+            httpDownloader.DownloadCompleted += HttpDownloader_DownloadCompleted;
+            httpDownloader.ProgressChanged += HttpDownloader_ProgressChanged;
+            httpDownloader.Start();
+        }
+
+        private void HttpDownloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            ProgressBar.Value = (int)e.Progress;
+            label_ProgressBar.Text = $"{e.Progress.ToString("0.00")}%";
+            label_Speed.Text = string.Format("{0} MB/s",(e.SpeedInBytes/ 1024d / 1024d).ToString("0.00"));
+            label_Downloded.Text = string.Format("{0} MB/s",(httpDownloader.TotalBytesReceived / 1024d / 1024d).ToString("0.00"));
+            label_Status.Text = "downloading...";
+
+        }
+
+        private void HttpDownloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate {
+                label_Status.Text = "Finish !";
+                label_ProgressBar.Text = "100 %";
+            });
+        }
+
+        private void guna2GradientButton2_Click(object sender, EventArgs e)
+        {
+            if (httpDownloader != null)
+            {
+                httpDownloader.Pause();
+            }
+        }
+
+        private void guna2GradientButton3_Click(object sender, EventArgs e)
+        {
+            if (httpDownloader != null)
+            {
+                httpDownloader.Resume();
+            }
+        }
+    }
+}
